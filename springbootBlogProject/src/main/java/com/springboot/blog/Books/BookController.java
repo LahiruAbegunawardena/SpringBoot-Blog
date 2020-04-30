7518511b8bece973dc7b8f05020f383f3e69456b
Unknown <lahirusabe15@gmail.com>
2020-04-30 11:08:53 +0530

    Blog article no 3 - Update,getById functionality

--- springbootBlogProject/src/main/java/com/springboot/blog/Books/BookController.java
+++ springbootBlogProject/src/main/java/com/springboot/blog/Books/BookController.java
@@ -1,9 +1,11 @@
 package com.springboot.blog.Books;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.constraints.Null;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("books")
@@ -12,12 +14,22 @@ public class BookController {
     private BookService bookService;
 
     @PostMapping("/store")
-    public BookModel storeBooks(@RequestBody BookModel newBook){
+    public Object storeBooks(@RequestBody BookModel newBook){
         return bookService.storeBooks(newBook);
     }
 
     @GetMapping("")
-    public Iterable<BookModel> getAllBooks(){
+    public Object getAllBooks(){
         return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{bookId}")
+    public Object getBookById(@PathVariable Integer bookId){
+        return bookService.getBookById(bookId);
+    }
+
+    @PutMapping("/update/{bookId}")
+    public Object updateBook(@PathVariable Integer bookId, @RequestBody BookModel updtBkDta){
+        return bookService.updateBook(bookId, updtBkDta);
     }
 }
--- springbootBlogProject/src/main/java/com/springboot/blog/Books/BookService.java
+++ springbootBlogProject/src/main/java/com/springboot/blog/Books/BookService.java
@@ -3,17 +3,95 @@ package com.springboot.blog.Books;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 @Service
 public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
 
-    public BookModel storeBooks(BookModel newBook){
-        return bookRepository.save(newBook);
+    public Object storeBooks(BookModel newBook){
+        Map<String, Object> responseData = new HashMap<>();
+        try {
+            BookModel newBookDta = bookRepository.save(newBook);
+            responseData.put("msg", "New Book stored successfully..");
+            responseData.put("status", true);
+            responseData.put("data", newBookDta);
+            return responseData;
+        } catch (Exception e){
+            responseData.clear();
+            responseData.put("msg", "New Book store failed..");
+            responseData.put("status", false);
+            responseData.put("data", null);
+            return  responseData;
+        }
     }
 
-    public Iterable<BookModel> getAllBooks(){
-        return bookRepository.findAll();
+    public Object getAllBooks(){
+        Map<String, Object> responseData = new HashMap<>();
+        try {
+            Iterable<BookModel> allBooks = bookRepository.findAll();
+            responseData.put("msg", "All books retrieved..");
+            responseData.put("status", true);
+            responseData.put("data", allBooks);
+            return responseData;
+        } catch (Exception e){
+            responseData.put("msg", "All books retrieve failed..");
+            responseData.put("status", false);
+            responseData.put("data", null);
+            return responseData;
+        }
+    }
+
+    public Object getBookById(Integer bookId){
+        Map<String, Object> responseData = new HashMap<>();
+        try {
+            if(bookRepository.existsById(bookId)){
+                Optional<BookModel> existDta = bookRepository.findById(bookId);
+                responseData.put("msg", "Book found by bookId");
+                responseData.put("status", true);
+                responseData.put("data", existDta);
+            } else {
+                responseData.clear();
+                responseData.put("msg", "There is no book exists by this id..");
+                responseData.put("status", false);
+                responseData.put("data", null);
+            }
+            return responseData;
+        } catch (Exception e){
+            responseData.clear();
+            responseData.put("msg", e.getMessage());
+            responseData.put("status", false);
+            responseData.put("data", null);
+            return  responseData;
+        }
+    }
+
+    public Object updateBook(Integer bookId, BookModel updtBkDta){
+        try {
+            Map<String, Object> responseData = new HashMap<>();
+
+            if(bookRepository.existsById(bookId)){
+                BookModel updatedDta = bookRepository.save(updtBkDta);
+                responseData.put("msg", "Book updated successfully");
+                responseData.put("status", true);
+                responseData.put("data", updatedDta);
+                return responseData;
+            } else {
+                responseData.put("msg", "There is no book exists by this id..");
+                responseData.put("status", false);
+                responseData.put("data", null);
+                return responseData;
+            }
+        } catch (Exception e){
+            Map<String, Object> responseData = new HashMap<>();
+            responseData.put("msg", e.getMessage());
+            responseData.put("status", false);
+            responseData.put("data", null);
+            return  responseData;
+        }
     }
 }
